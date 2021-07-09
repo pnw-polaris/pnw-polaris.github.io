523ac142de06f462e81d8850c969e3aa21bb28a8
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-07-09 14:05:39 -0700

    Fixing menu handler

---
+++ static/core.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  var x = document.getElementById("main_menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
